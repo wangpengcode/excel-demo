b7da9703d67e8dd956fbec8a6f13a671c17543f6
wangpeng2 <wangpeng2@ushareit.com>
2023-07-18 10:28:29 +0800

    # 生成SQL

--- src/test/kotlin/com/wangpeng/excel/ExcelApplicationTests.kt
+++ src/test/kotlin/com/wangpeng/excel/ExcelApplicationTests.kt
@@ -2,12 +2,32 @@ package com.wangpeng.excel
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.FileReader
+import java.io.FileWriter
+
 
 @SpringBootTest
 class ExcelApplicationTests {
 
     @Test
     fun contextLoads() {
+        val reader = BufferedReader(FileReader("/Users/wangpeng/Documents/工作相关/卡bin服务/Archive/1.csv"))
+        var line: String? = null
+        var sql: String? = null
+        val out = BufferedWriter(FileWriter("1.sql",true))
+        while ((reader.readLine().also { line = it }) != null) {
+            sql = ""
+            for (text in line!!.split(",")) {
+                println(text)
+                sql += "$text,"
+            }
+            out.write(sql)
+            break
+        }
+        reader.close()
+        out.close()
     }
 
 }
