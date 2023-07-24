40aee9e2e8f26ca8ea95adda3bd9cfc287b50285
wangpeng2 <wangpeng2@ushareit.com>
2023-07-24 15:31:34 +0800

    # 生成SQL

--- src/test/kotlin/com/wangpeng/excel/ExcelApplicationTests.kt
+++ src/test/kotlin/com/wangpeng/excel/ExcelApplicationTests.kt
@@ -1,6 +1,7 @@
 package com.wangpeng.excel
 
 import org.junit.jupiter.api.Test
+import org.junit.platform.commons.util.StringUtils
 import org.springframework.boot.test.context.SpringBootTest
 import java.io.BufferedReader
 import java.io.BufferedWriter
@@ -16,18 +17,35 @@ class ExcelApplicationTests {
         val reader = BufferedReader(FileReader("/Users/wangpeng/Documents/工作相关/卡bin服务/Archive/1.csv"))
         var line: String? = null
         var sql: String? = null
-        val out = BufferedWriter(FileWriter("1.sql",true))
-        while ((reader.readLine().also { line = it }) != null) {
-            sql = ""
-            for (text in line!!.split(",")) {
-                println(text)
-                sql += "$text,"
+        val out = BufferedWriter(FileWriter("/Users/wangpeng/Documents/工作相关/卡bin服务/Archive/1.sql", true))
+        try {
+            while ((reader.readLine().also { line = it }) != null) {
+                sql = "INSERT INTO fintech_cashier.cy_cardbin_raw_data (start_card_bin_field_1, end_card_bin_field_2, maybe_country_field_3, card_brand_field_4, issuer_field_5, country_field_6, country_code_field_7, country_field_8, field_9, currency_field_10, ddc_field_11, field_12, field_13, field_14, field_15, field_16) VALUES("
+                val list = line!!.split(",")
+                if (list.size < 16) {
+                    println("error ++++++++++ list size below 16")
+                    continue
+                }
+                for (i in 1 until list.size) {
+                    sql += if (StringUtils.isBlank(list[i])) {
+                        null + ","
+                    } else if (i < 16){
+                        "'"+list[i] + "',"
+                    } else {
+                        "'"+list[i] + "'"
+                    }
+                }
+                sql +="); \n"
+                out.write(sql)
+                out.flush()
             }
-            out.write(sql)
-            break
+
+        } catch (e: Exception) {
+            println(e)
+        } finally {
+            out.close()
+            reader.close()
         }
-        reader.close()
-        out.close()
     }
 
 }
